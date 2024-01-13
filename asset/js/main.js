74c42a5ad2105d0d186aa99d9321bd7770f9a229
ryoto_hiji <ryotohiji.41@gmail.com>
2024-01-13 12:03:09 +0900

    ・ヘッダー部分完成 ・gitignoreファイルの導入

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,17 @@
 'use strict';
 
-//ナビゲーション
-//ナビゲーション>ホバーした際の挙動
+var hamburger = document.querySelector('.header__hamburger-menu');
+var navigation = document.querySelector('.navigation');
+var hamburgerLines = document.querySelectorAll('.header__hamburger-menu__line');
+
+
+
+//ナビゲーション:クリック時の挙動
+hamburger.addEventListener('click',function(){
+    navigation.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    hamburgerLines.forEach(function(hamburgerLine){
+        hamburgerLine.classList.toggle('active');
+    })
+});
+
