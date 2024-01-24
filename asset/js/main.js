24e2b3d2accc8a23cb0e96ba234e684b30b7e136
ryoto_hiji <ryotohiji.41@gmail.com>
2024-01-24 21:20:42 +0900

    読み込み時にプログレスバーを表示

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,13 +2,47 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+
+////ローディングアニメーション
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 1000,//時間指定(1000＝1秒)
+	strokeWidth: 0.2,//進捗ゲージの太さ
+	color: '#555',//進捗ゲージのカラー
+	trailWidth: 0.2,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1rem',
+			color: '#333333',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});  
+
+
 var hamburger = document.querySelector('.header__hamburger-menu');
 var navigation = document.querySelector('.navigation');
 var hamburgerLines = document.querySelectorAll('.header__hamburger-menu__line');
 
 
 
-//ナビゲーション:クリック時の挙動
+////ナビゲーション:クリック時の挙動
 hamburger.addEventListener('click',function(){
     navigation.classList.toggle('active');
     hamburger.classList.toggle('active');
@@ -17,7 +51,7 @@ hamburger.addEventListener('click',function(){
     })
 });
 
-//メニューのスムーススクロール
+////メニューのスムーススクロール
 //header,footer,Service>ご相談
 document.addEventListener("DOMContentLoaded", function() {
     const menuLinks = document.querySelectorAll('.header__menu a');
